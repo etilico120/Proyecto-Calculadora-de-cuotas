ef53f08d93aff76ec78b156ee4010fb64464c94d
Juan Rosano <juanrosano@gmail.com>
2022-12-16 22:08:34 -0300

    Correciones Clase Final-16122022

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue';
-const { default: swal } = require("sweetalert");
-const { setActionValue } = require("sweetalert/typings/modules/state");
+// import { createApp } from 'vue';
+//const { default: swal } = require("sweetalert2");
+//const { setActionValue } = require("sweetalert2/typings/modules/state");
+//import Swal from 'sweetalert2'
 
 
 const app = Vue.createApp({
@@ -9,20 +10,28 @@ const app = Vue.createApp({
         item: null,
         fecha: null,
         error: null,
-        meses: null,
-        anios: null,
         anio: null,
-        mes: null
+        anios: null,
+        meses: null,
+        mes: null,
+        total: null,
+        min: null
       }
     },
     computed:{
         fechaMinima(){
             let hoy = new Date();
-            let dia = hoy.getDate();
-            let mes = hoy.getMonth();
-            let anio = hoy.getFullYear();
-            return hoy = dia + '/' + mes + '/' + anio;
-        }
+            let dd = hoy.getDate();
+            let mm = hoy.getMonth()+1;
+            let aaaa = hoy.getFullYear();
+            if (dd<10){
+                dd = '0' + dd;
+            }
+            if (mm<10){
+                mm = '0' + mm;
+            }
+            return hoy = aaaa + '-' + mm + '-' + dd;
+        },
     },
     methods:{
         obtenerAnios(){
@@ -59,29 +68,65 @@ const app = Vue.createApp({
             })
         },
         calculo(){
-            if ((!this.anio) && (!this.mes) && (!this.dia)){
+            // const self = this;
+            if ((!this.anio)){
                 swal.fire({
                     title: 'Error!',
                     text:  'Seleccioná un año',
-                    icon: 'information'
+                    icon: 'info'
                 })
                 return;
+            }
+            if ((!this.mes)){
+                swal.fire({
+                    title: 'Error!',
+                    text:  'Seleccioná un mes',
+                    icon: 'info'
+                })
+                return;
+            }
+            if ((!this.fecha)){
+                swal.fire({
+                    title: 'Error!',
+                    text:  'Seleccioná un día',
+                    icon: 'info'
+                })
+                return;
+            }
+            this.total = 0;
+            let mesSeleccionado = this.mes;
+            switch (this.anio){
+                case "primero":
+                    this.total = this.datosObtenidos.primero[mesSeleccionado];
+                    this.calculoConAumento();
+                    break;
+                case "segundo":
+                    this.total = this.datosObtenidos.segundo[mesSeleccionado];
+                    this.calculoConAumento();
+                    break;
+                case "tercero":
+                    this.total = this.datosObtenidos.tercero[mesSeleccionado];
+                    this.calculoConAumento();
+                    break;
+                case "cuarto":
+                    this.total = this.datosObtenidos.cuarto[mesSeleccionado];
+                    this.calculoConAumento();
+                    break;
             }
         },
         calculoConAumento(){
             let total = 0;
             let hoy = new Date();
             let fechaSeleccionada = new Date(this.fecha);
-            let diferencia = new Date((fechaSeleccionada-fecha)).getDate()
+            let diferencia = new Date((fechaSeleccionada-this.fecha)).getDate()
             if (diferencia > 15){
                 this.total = this.total + 400
             }
-        },
+        }
     },
     mounted(){
-        let vAnios = obtenerAnios();
-        console.log(vAnios);
-        obtenerMeses();
-        datos();
+        this.obtenerAnios();
+        this.obtenerMeses();
+        this.datos();
     }
 })
